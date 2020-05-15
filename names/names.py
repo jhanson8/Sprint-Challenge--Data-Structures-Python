4668230a2bd6dc6bc8717144c177802e55464556
Jonathan Hanson <jhanson8@uoregon.edu>
2020-05-15 09:10:09 -0700

    bst, reveres edit

--- names/names.py
+++ names/names.py
@@ -1,4 +1,8 @@
 import time
+from collections import Counter
+from doubly_linked_list import DoublyLinkedList as dll
+from binary_search_tree import BSTNode as bst 
+
 
 start_time = time.time()
 
@@ -13,10 +17,48 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
+#quadratic (On^c) 
+'''for name_1 in names_1:
     for name_2 in names_2:
         if name_1 == name_2:
-            duplicates.append(name_1)
+            duplicates.append(name_1)'''
+
+#set method takes in an iterable list, string tuple, etc. Outputs as object { }
+#intersection method takes in a list and compares to another list and returns an object with of all of the common elements between the two lists  
+
+'''d = set(names_1).intersection(names_2)
+for i in d: 
+    duplicates.append(i)'''
+
+'''for i in [x for x in names_1 if x in names_2]:
+    duplicates.append(i)'''
+
+#bst data structure 
+bst = bst(names_1[0])
+for x in names_1:
+    bst.insert(x)
+for i in names_2:
+    if bst.contains(i):
+        duplicates.append(i)
+print(len(duplicates))
+
+#doubly
+'''
+d = dll(names_1[0])
+for x in names_1:
+    d.add_to_head(x)
+
+for i in names_2:
+    if d.contains(i):
+        duplicates.append(i)'''
+
+
+
+    
+
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
@@ -24,5 +66,5 @@ print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
-# What's the best time you can accomplish?  Thare are no restrictions on techniques or data
+# What's the best time you can accomplish?  There are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
